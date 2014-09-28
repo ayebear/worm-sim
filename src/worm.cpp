a31f39f3c2faef17db9d36b8b8d5f02a2e258ae2
Conor McCarthy <cpmcc94@gmail.com>
2014-09-27 21:30:25 -0400

    added worm class

--- src/worm.cpp
+++ src/worm.cpp
@@ -1,11 +1,47 @@
 #include "worm.h"
 
-Worm::Worm()
+Worm::Worm() :
+    wormBody(sf::TrianglesStrip, 100)
 {
     //ctor
+
+    for(int i =0; i < 100; i++){
+        if(i%2==0){
+            wormBody[i].position = sf::Vector2f(i*10, 200);
+            wormBody[i].color = sf::Color::Red;
+        }
+        else{
+            wormBody[i].position = sf::Vector2f(i*10, 100);
+            wormBody[i].color = sf::Color::Red;
+        }
+        //wormBody[i].texCoords = sf::Vector2f(100, 100);
+    }
+
+    collided = false;
+
+
+
+}
+
+void Worm::UpdateWorm(sf::Vector2f position){
+    wormBody[0].position = position;
+    for(int i = 0; i < 100; i++)
+    {
+
+    }
 }
 
 Worm::~Worm()
 {
     //dtor
+}
+
+void Worm::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+// You can draw other high-level objects
+
+// ... or use the low-level API
+//states.texture = &m_texture;
+target.draw(wormBody, states);
+
 }
--- src/worm.h
+++ src/worm.h
@@ -1,14 +1,22 @@
 #ifndef WORM_H
 #define WORM_H
 
-
-class Worm
+#include <vector>
+#include <SFML/Graphics.hpp>
+class Worm : public sf::Drawable
 {
     public:
         Worm();
         virtual ~Worm();
+        void UpdateWorm(sf::Vector2f position);
+        void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     protected:
+
     private:
+        sf::VertexArray wormBody;
+        bool collided;
+
+        //int bodySize;
 };
 
 #endif // WORM_H
